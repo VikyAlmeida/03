dcf252608882608eb855280f65ebaf9d5e868dfb
VikyAlmeida <viky2000.22@gmail.com>
2023-06-05 05:03:23 +0200

    dividir el menu

--- views/modules/partials/scripts.php
+++ views/modules/partials/scripts.php
@@ -9,6 +9,8 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	<script>
+		
+		// datatables
 		$(document).ready(function() {
 			$('#tableAdminUsers').DataTable( {
 				"responsive" : true,
@@ -33,5 +35,104 @@
 				}
 			} );
 		} );
-	</script>
-	
+
+		// secciones del menu
+		const partial = localStorage.getItem('partial');
+		if (partial === null) localStorage.setItem('partial', 'statistics');
+		document.getElementById(partial).style.display = "flex";
+
+		function getPartial (value) {
+			console.log('hola');
+			const partial =  localStorage.getItem('partial');
+			localStorage.setItem("partial", value);
+			location.reload();
+
+			document.getElementById('statistics').style.display = "none";
+			document.getElementById(partial).style.display = "none";
+			document.getElementById(value).style.display = "flex";
+		}
+
+		// graficas
+		const grafica1 = document.getElementById('myChart1');
+		const grafica2 = document.getElementById('myChart2');
+		const grafica3 = document.getElementById('myChart3');
+
+		new Chart(grafica1,{
+			type:'doughnut',
+			data:{
+				labels:['Administrador','Propietario','Usuario'],
+				datasets:[{
+					label: 'num',
+					data: [1, 2, 3], 
+					backgroundColor: [
+						'hsla(347, 100%, 92%, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+					],
+				}]
+			},
+			options: {
+				resposive:true
+			}
+		});
+		new Chart(grafica2,{
+			type:'doughnut',
+			data:{
+				labels:['Administrador','Propietario','Usuario'],
+				datasets:[{
+					label: 'num',
+					data: [1, 2, 3], 
+					backgroundColor: [
+						'hsla(347, 100%, 92%, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+					],
+				}]
+			},
+			options: {
+				resposive:true
+			}
+		});
+		new Chart(grafica3, {
+			type: 'scatter',
+			data: {
+				labels: [
+					'January',
+					'February',
+					'March',
+					'April'
+				],
+				datasets: [{
+					type: 'bar',
+					label: 'Bar Dataset',
+					data: [10, 20, 30, 40],
+					borderColor: 'rgb(255, 99, 132)',
+					backgroundColor: 'rgba(255, 99, 132, 0.2)'
+				}, {
+					type: 'line',
+					label: 'Line Dataset',
+					data: [10, 50, 50, 50],
+					fill: false,
+					borderColor: 'rgb(54, 162, 235)'
+				}]
+				},
+			options: {
+				scales: {
+				y: {
+					beginAtZero: true
+				}
+				}
+			}
+		});
+
+</script>
