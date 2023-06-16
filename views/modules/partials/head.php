d5d4dfd114f837510cc878c7216947ef8ca01a0e
VikyAlmeida <viky2000.22@gmail.com>
2023-06-16 06:46:39 +0200

    FIX

--- views/modules/partials/head.php
+++ views/modules/partials/head.php
@@ -24,10 +24,9 @@
 	<link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
 
-	<!--[if lt IE 9]>
-	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
 	<style>
     .btn-form-styled {
         height: 56px;
